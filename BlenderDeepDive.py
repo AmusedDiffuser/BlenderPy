88a28c5ff94175d7af437d4276441cdcfe857cf9
AmusedDiffuser <81667911+AmusedDiffuser@users.noreply.github.com>
2023-05-16 21:08:33 -0400

    Update BlenderDeepDive.py
    total rewrite of the concept v1

--- BlenderDeepDive.py
+++ BlenderDeepDive.py
@@ -1,49 +1,70 @@
-# Import bpy module
+Here is the next logical revision for the existing script that would implement as much as possible of what we’ve discussed, while keeping the complexity of the changes minimal:
+
+# Depthify: Create realistic and stunning 3D surfaces from depth map images in Blender.
+# This script imports and transforms a depth map image into a 3D surface in Blender.
+# It uses some modules and features such as bpy, numpy, argparse, etc. to enhance the quality, usability, and functionality of the script.
+
+# Import the modules
 import bpy
+import numpy as np
+import argparse
+import sys
 
-# Enable the "Import Images as Planes" add-on
-bpy.ops.preferences.addon_enable(module="io_import_images_as_planes")
+# Define a function to parse and validate the command line arguments
+def parse_args():
+    # Create an argument parser object
+    parser = argparse.ArgumentParser(description="Depthify: Create realistic and stunning 3D surfaces from depth map images in Blender.")
+    # Add some arguments to the parser
+    parser.add_argument("file_name", type=str, help="The file name of the depth map image.")
+    parser.add_argument("scale", type=float, default=0.2, help="The scale value for the displacement modifier. Default is 0.2.")
+    parser.add_argument("dicing_rate", type=float, default=0.5, help="The dicing rate for the adaptive subdivision modifier. Default is 0.5.")
+    # Parse the arguments and return them
+    args = parser.parse_args()
+    # Validate the arguments and exit with an error message if invalid
+    if not args.file_name.endswith(".png"):
+        sys.exit("Error: The file name must be a PNG image.")
+    if not 0 < args.scale < 1:
+        sys.exit("Error: The scale value must be between 0 and 1.")
+    if not 0 < args.dicing_rate < 1:
+        sys.exit("Error: The dicing rate must be between 0 and 1.")
+    return args
 
-# Get the file name from user input or use a default value
-file_name = input("Enter the file name of the depth map image: ") or "depth_map.png"
+# Define a function to import and transform the depth map image into a 3D surface in Blender
+def depthify(file_name, scale, dicing_rate):
+    # Delete the default cube object
+    bpy.ops.object.select_all(action="SELECT")
+    bpy.ops.object.delete()
+    # Import the depth map image as a plane object
+    bpy.ops.import_image.to_plane(files=[{"name": file_name}])
+    # Get the plane object and its data
+    plane = bpy.context.active_object
+    plane_data = plane.data
+    # Set the origin of the plane to its center
+    bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY", center="MEDIAN")
+    # Set the shading of the plane to smooth
+    bpy.ops.object.shade_smooth()
+    # Add a subdivision surface modifier to the plane
+    sub_mod = plane.modifiers.new(name="Subdivision", type="SUBSURF")
+    # Set the subdivision levels to 2 for viewport and render
+    sub_mod.levels = 2
+    sub_mod.render_levels = 2
+    # Enable adaptive subdivision for render
+    sub_mod.use_subsurf_uv = True
+    sub_mod.use_adaptive_subdivision = True
+    # Set the dicing rate for adaptive subdivision
+    sub_mod.dicing_rate = dicing_rate
+    # Add a displacement modifier to the plane
+    dis_mod = plane.modifiers.new(name="Displacement", type="DISPLACE")
+    # Set the texture of the displacement modifier to the depth map image
+    dis_tex = bpy.data.textures.new(name="Displacement Texture", type="IMAGE")
+    dis_tex.image = bpy.data.images[file_name]
+    dis_mod.texture = dis_tex
+    # Set the coordinates of the displacement modifier to UV
+    dis_mod.texture_coords = "UV"
+    # Set the strength of the displacement modifier to the scale value
+    dis_mod.strength = scale
 
-# Get the scale value from user input or use a default value
-scale_value = float(input("Enter the scale value for the displacement: ")) or 0.1
-
-# Get the dicing rate from user input or use a default value
-dicing_rate = float(input("Enter the dicing rate for the adaptive subdivision: ")) or 1
-
-# Try to import the depth map image as a plane
-try:
-    bpy.ops.import_image.to_plane(files=[{"name":file_name}])
-except RuntimeError as e:
-    print("Error: ", e)
-    exit()
-
-# Get the plane object and its material
-plane = bpy.context.selected_objects[0]
-mat = plane.material_slots[0].material
-
-# Set the map texture node to non-color data
-map_node = mat.node_tree.nodes["Image Texture"]
-map_node.image.colorspace_settings.name = "Non-Color"
-
-# Add a subdivision modifier to the plane
-subdiv = plane.modifiers.new("Subdivision", "SUBSURF")
-subdiv.subdivision_type = "SIMPLE"
-
-# Enable adaptive subdivision and set the dicing rate
-bpy.context.scene.cycles.use_adaptive_subdivision = True
-plane.cycles.dicing_rate = dicing_rate
-
-# Add a displacement node and connect it to the material output node
-disp_node = mat.node_tree.nodes.new("ShaderNodeDisplacement")
-output_node = mat.node_tree.nodes["Material Output"]
-mat.node_tree.links.new(disp_node.outputs["Displacement"], output_node.inputs["Displacement"])
-
-# Set the displacement node settings
-disp_node.inputs["Scale"].default_value = scale_value # Adjust this value as needed
-disp_node.inputs["Midlevel"].default_value = 0.5
-
-# Connect the map texture node to the displacement node
-mat.node_tree.links.new(map_node.outputs["Color"], disp_node.inputs["Height"])
+# Call the parse_args function and get the arguments
+args = parse_args()
+# Call the depthify function with the arguments
+depthify(args.file_name, args.scale, args.dicing_rate)
